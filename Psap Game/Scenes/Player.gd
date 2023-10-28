5fcdef210a9ff57a1a8cd2f857e0d0692bd9d33c
Otinane5 <67025219+Otinane5@users.noreply.github.com>
2023-10-28 22:32:11 +0300

    Δημιούργησα CharacterParent
    Δημιούργησα ένα scene CharacterParent  ώστε όλοι οι χαρακτήρες του game να το κάνουν inherit και ετσι να εχουν hp,speed etc. Ετσι έβαλα και τον player να κάνει inherit αυτο το scene το οποίο έχει και ένα hit() function ωστε όλοι να μπορούν να φάνε dmg με knockback

--- Psap Game/Scenes/Player.gd
+++ Psap Game/Scenes/Player.gd
@@ -1,12 +1,10 @@
-extends CharacterBody2D
+extends CharacterParent
 
+func _ready():
+	SPEED = 300.0
+	JUMP_VELOCITY = -500.0
+	health = 100
 
-var SPEED = 300.0
-var JUMP_VELOCITY = -400.0
-var direction
-var health = 100
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func collect(itemType, value):
 	if itemType=="healthPotion":
@@ -17,7 +15,7 @@ func collect(itemType, value):
 		SPEED*= 2
 		$BoostTimer.start()
 
-func _on_boost_timer_timeout():     #Αναιρεί το Boost στο τέλος του χρόνου
+func _on_boost_timer_timeout():
 	SPEED /= 2
 
 func _physics_process(delta):
@@ -25,17 +23,15 @@ func _physics_process(delta):
 	if not is_on_floor():            # Add the gravity.
 		velocity.y += gravity * delta
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor(): # Handle Jump.
-		velocity.y = JUMP_VELOCITY
-	
-	# Get the input direction and handle the movement/deceleration.
-	direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	if movementEnable==true:
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor(): # Handle Jump.
+			velocity.y = JUMP_VELOCITY
+		
+		# Get the input direction and handle the movement/deceleration.
+		direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-
-
---
+++ Psap Game/Scenes/character_parent.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+class_name CharacterParent
+
+var SPEED = 300.0
+var JUMP_VELOCITY = -500.0
+var direction
+var health = 100
+var movementEnable = true    #Eπιτρέπει την κίνηση όταν είναι true
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func hit(dmg,knockback, dmg_Vector2):   #Ειναι το function για να δέχονται dmg και knockback οι χαρακτήρες
+	movementEnable = false
+	health= health-dmg
+	velocity = Vector2(knockback,knockback)*(position-dmg_Vector2)
+	$KnockbackTimer.start()
+
+
+func _on_knockback_timer_timeout():
+	movementEnable=true
